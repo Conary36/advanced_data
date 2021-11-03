bec8884637a6efda0c57013e4feaa009ceb644f1
Conary36 <crossingwaters@icloud.com>
2021-11-02 22:29:11 -0400

    script.js added

---
+++ script.js
@@ -0,0 +1,55 @@
+const resultContents = document.querySelector(".resultContents");
+
+function loadData () {
+fetch("https://genius.p.rapidapi.com/artists/16775/songs", {
+  method: "GET",
+  headers: {
+    "x-rapidapi-host": "genius.p.rapidapi.com",
+    "x-rapidapi-key": "93bf4d306dmshd13cc3fe1547f3ep164f1djsnb836e94ce652",
+  },
+})
+.then(res => res.json())
+  .then((response) => {
+    console.log(response);
+    displayData(response.response.songs)
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+}
+
+function displayData (songs) {
+
+    songs.forEach(song => {
+        // const songTitle = document.createElement("h2");
+        // songTitle.innerText = song.title;
+        // resultContents.appendChild(songTitle);
+        // const songLyrics = document.createElement("p");
+        // songLyrics.innerText = song.lyrics;
+        // resultContents.appendChild(songLyrics);
+        const imgContainer = document.createElement("div");
+        imgContainer.classList.add("resultImage");  
+        const image = document.createElement("img"); 
+        image.src = song.song_art_image_thumbnail_url;  
+        imgContainer.appendChild(image);
+
+        const resultText = document.createElement("div");
+        resultText.classList.add("resultText");
+        const songTitle = document.createElement("p");
+        songTitle.innerText = song.title;
+        resultText.appendChild(songTitle);
+
+        resultContents.appendChild(imgContainer);
+        resultContents.appendChild(resultText);
+    })
+
+//      <div class="resultImage">
+//               <img src="" alt="artist">
+//       </div>
+//        <div class="resultText">
+//              <p class="resultDescription"></p>
+//       </div>
+}
+
+
+loadData()
--- server.js
+++ server.js
@@ -1,17 +1,32 @@
 // server.js
 const express = require('express');
 // const bodyParser = require("body-parser"); deprecated
+const axios = require("axios");
 const app = express();
+const CORS = require('cors');
+
 //MIDDLEWARE
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());// To parse the incoming requests with JSON payloads
+app.use(CORS());
+
+
+// function musicData(){
+//     axios.get("https://genius.p.rapidapi.com/artists/16775/songs")
+//     .then(res => {
+//         console.log(res.data.response.songs);
+//     })
+//     .catch(err => console.log(err));
+
+// }
+// musicData();
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
-// app.get('/api/music', (req, res) => {
-//     console.log('Helloooooooooooooooooo!')
-// });
+app.get('/api/music', (req, res) => {
+    console.log('Helloooooooooooooooooo!')
+});
 
 
 
